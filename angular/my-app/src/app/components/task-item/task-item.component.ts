6a38eaf0293d91c810a4ec88c6acf3044968f467
Lia Cicati <LiaCicati@yandex.com>
2022-07-12 13:22:50 +0200

    implemented: delete task

--- angular/my-app/src/app/components/task-item/task-item.component.ts
+++ angular/my-app/src/app/components/task-item/task-item.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent implements OnInit {
   @Input()
   task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDelete(task: any) {
+    this.onDeleteTask.emit(task);
   }
-
 }
