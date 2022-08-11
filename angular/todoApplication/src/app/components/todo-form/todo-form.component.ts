8e937f7353908e10d4dbee73dcaae56dc2fb8946
Lia Cicati <LiaCicati@yandex.com>
2022-08-11 12:38:50 +0300

    refactoring

--- angular/todoApplication/src/app/components/todo-form/todo-form.component.ts
+++ angular/todoApplication/src/app/components/todo-form/todo-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, EventEmitter, OnInit, Output, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 import { InputComponent } from '../input/input.component';
@@ -13,19 +17,16 @@ import { TodoItem } from '../../interfaces/todo-item';
   styleUrls: ['./todo-form.component.scss'],
 })
 export class TodoFormComponent {
-public taskName!:string;
+  public taskName!: string;
 
-  @ViewChild("myForm")
-  private readonly _form!:NgForm;
-
-
+  // @ViewChild("myForm")
+  // private readonly _form!:NgForm;
   @Output() onSubmitForm: EventEmitter<any> = new EventEmitter();
   constructor() {}
   value?: TodoItem;
 
   public onSubmit(form: FormGroup) {
     this.onSubmitForm.emit(form.value.taskTitle);
-    this._form.reset(); //TODO: see why it's not working
-    // this.taskName='';
+    form.reset();
   }
 }
